ac6669e4a475ff2c81beeb3ead1c2706ee1f4554
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-08-14 17:33:36 +0530

    added map component to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MapComponent from './components/MapComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-3 items-center justify-center w-full">
+      <h1 className='text-3xl mt-5 font-bold' >Vehicle Movement on Map</h1>
+      <p>Click on any two point on the map and wait for 7-8 seconds the Route will be generated. You can clear the route as well by clicking on Reset the Route button.</p>
+      <MapComponent/>
     </div>
   );
 }
